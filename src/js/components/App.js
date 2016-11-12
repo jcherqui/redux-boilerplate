24cec0014636f66f457e81f672b54e10ac2302fb
ston3o <5039608+ston3o@users.noreply.github.com>
2016-11-12 18:09:43 +0100

    Add redux

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from '../containers/user-list';
+
+const App = () => {
+    return (
+        <div>
+            <div>Hello World!</div>
+            <UserList />
+        </div>
+    );
+};
+
+export default App;
